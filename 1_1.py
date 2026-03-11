663ef85e578317939581aab08f4192f974d5e6be
ryo-h47 <ryotarohama2005@icloud.com>
2026-03-11 14:58:05 +0900

    1_1.py
    Python exercise from the first book

---
+++ 1_1.py
@@ -0,0 +1,8 @@
+name=input("What is your name?")
+country=input("Where are you from?")
+major=input("What is your major?")
+age=input("How old are you?")
+cm=input("How tall are you?")
+inch=float(cm)/2.54
+
+print("Hello, I'm " + name + ". I'm from " + country + ". I'm majoring in " + major + ". I'm " + age + " years old. I'm " + str(inch) + " inches tall. Nice to meet you!")
