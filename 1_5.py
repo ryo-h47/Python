9881c45e590aa3c55cfc92a5b6bcf2f758492f52
ryo-h47 <ryotarohama2005@icloud.com>
2026-03-15 21:01:51 +0900

    1_5.py
    Python exercise from the first book

---
+++ 1_5.py
@@ -0,0 +1,18 @@
+s="""
+サンマ , カツオ , サンマ , サンマ , マグロ , フグ , マグロ , マグロ , マグロ , サンマ , ニシン , イワシ ,
+サンマ , サンマ , カツオ , サンマ , カツオ , サンマ , カツオ , サンマ , マグロ , マグロ , マグロ , ニシン
+"""
+
+s=s.strip()
+
+s_list=s.split(",")
+
+result={}
+for name in s_list:
+    name=name.strip()
+    if not name in result:
+        result[name]=0
+    result[name]+=1
+
+for name, v in result.items():
+    print(name + " = " + str(v))
